45553062fb7ce288e82509d785ee9b6e823d7834
julianjchen <julianjchen@tencent.com>
2024-01-27 19:24:11 +0800

    add

---
+++ convert6.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "PAYPALISHIRING"
+	numRows := 3
+	fmt.Println(convert(str, numRows))
+}
+
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	mod := numRows*2 - 2
+	arr := make([]string, numRows)
+
+	for i, char := range s {
+		x := i % mod
+		index := min(x, mod-x)
+		arr[index] += string(char)
+	}
+
+	return strings.Join(arr, "")
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
