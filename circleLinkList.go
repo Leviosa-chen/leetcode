a30af7f3bfa9bea1326ad1756baf758fd7dd33b6
julianjchen <julianjchen@tencent.com>
2024-10-14 23:03:24 +0800

    qsort

---
+++ circleLinkList.go
@@ -0,0 +1,54 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+
+	node1 := &ListNode{Val: 1}
+	node2 := &ListNode{Val: 2}
+	node3 := &ListNode{Val: 3}
+	node4 := &ListNode{Val: 4}
+	node1.Next = node2
+	node2.Next = node3
+	node3.Next = node4
+
+	head := node1
+
+	head = reverseList(head)
+	// 输出
+	current := head
+	for current != nil {
+		println(current.Val)
+		current = current.Next
+	}
+
+}
+
+// 迭代法
+func reverseList1(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	var pre *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = pre
+		pre = head
+		head = next
+	}
+	return pre
+}
+
+// 递归法
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+}
